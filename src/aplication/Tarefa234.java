78787b9c6edee248b87825d06b6f89780fa2a8a9
marci <marci@DESKTOP-K18THHT>
2020-03-02 15:34:15 -0300

    tarefa-merge

--- src/aplication/Tarefa234.java
+++ src/aplication/Tarefa234.java
@@ -5,6 +5,8 @@ public class Tarefa234 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("tarefa234");
+		System.out.println("tarefa234");
+		System.out.println("tarefa234");
 	}
 
 }
